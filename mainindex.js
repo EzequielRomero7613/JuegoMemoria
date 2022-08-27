861143fee3aed5b4ee6f84a0cee147c9373cdcc5
EzequielRomero7613 <61565104+EzequielRomero7613@users.noreply.github.com>
2022-08-27 19:36:12 -0300

    Update mainindex.js

--- mainindex.js
+++ mainindex.js
@@ -2,14 +2,14 @@
 function facil(){
     onclick = function(){
         document.getElementById("facil");
-        this.location.href ="../juegofacil.html";
+        this.location.href ="./juegofacil.html";
     }
 }
 
 function media(){
     onclick = function(){
         document.getElementById("media");
-        this.location.href ="../juegomedio.html";
+        this.location.href ="./juegomedio.html";
     }
 }
 
@@ -17,6 +17,6 @@ function media(){
 function dificil(){
     onclick = function(){
         document.getElementById("dificil");
-        this.location.href ="../juegodificil.html";
+        this.location.href ="./juegodificil.html";
     }
 }
